5b8ee79817437e2a5927c9b31aa8cd596074d8ff
yoncola1 <yoncola@yandex.ru>
2026-02-22 21:47:23 +0400

    Update: Added Plezix start page and rebranding

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -316,7 +316,7 @@ pref("browser.shell.setDefaultGuidanceNotifications", true);
 // 0 = blank, 1 = home (browser.startup.homepage), 2 = last visited page, 3 = resume previous browser session
 // The behavior of option 3 is detailed at: http://wiki.mozilla.org/Session_Restore
 pref("browser.startup.page",                1);
-pref("browser.startup.homepage",            "about:home");
+pref("browser.startup.homepage",            "chrome://browser/content/new_tab_page.html");
 pref("browser.startup.homepage.abouthome_cache.enabled", true);
 pref("browser.startup.homepage.abouthome_cache.loglevel", "Warn");
 
@@ -3386,7 +3386,7 @@ pref("startup.homepage_override_url_nimbus", "");
 pref("startup.homepage_override_nimbus_maxVersion", "");
 pref("startup.homepage_override_nimbus_minVersion", "");
 // Pref to disable all What's New pages
-pref("startup.homepage_override_nimbus_disable_wnp", false);
+pref("startup.homepage_override_nimbus_disable_wnp", true);
 
 // Pref to enable the content relevancy feature.
 pref("toolkit.contentRelevancy.enabled", false);
